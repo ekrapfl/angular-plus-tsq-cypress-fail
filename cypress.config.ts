1ad73ba804517418374dd2a6d166ae846665e7e0
Eben Krapfl <ekrapfl@opengov.com>
2024-09-30 16:38:19 -0500

    Minimize extra config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "cypress";
-import * as path from "path";
 
 export default defineConfig({
   viewportHeight: 600,
@@ -8,15 +7,6 @@ export default defineConfig({
     devServer: {
       framework: "angular",
       bundler: "webpack",
-      webpackConfig: {
-        devServer: {
-          static: {
-            // Serve images with absolute paths under /assets
-            directory: path.join(__dirname, "src/assets"),
-            publicPath: "/assets/",
-          },
-        },
-      },
     },
     specPattern: "**/*.cy.ts",
   },
